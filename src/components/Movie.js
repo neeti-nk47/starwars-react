fc57387511864e0af5fbffbda7b024103599eb16
neeti-nk47 <neeti.nk47@gmail.com>
2023-06-01 23:17:21 +0530

    adding delete button

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
 const Movie = (props) => {
+  const deleteHandler = () => {
+    fetch(
+      `https://starwars-react-19e97-default-rtdb.asia-southeast1.firebasedatabase.app/movies/${props.id}.json`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((res) => res.text()) // or res.json()
+      .then((res) => console.log(res));
+  };
+
   return (
     <li className={classes.movie}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <button onClick={deleteHandler}>Delete</button>
     </li>
   );
 };
